df7c8822c696ffb939aec1c3a9913ef76df1bc65
Ratman <wurenmooncake@gmail.com>
2026-05-14 22:27:27 +0800

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        def numToStr(num: int) -> str:
+            if num > 0:
+                return '+' + str(num)
+            else:
+                return '-' + str(abs(num))
+
+        nums = sorted(nums)
+        res = []
+        resHash: dict[str, bool] = {}
+        print(nums)
+
+        for i in range(len(nums) - 2):
+            [j, k] = [i + 1, len(nums) - 1]
+
+            while j < k:
+                # nums[j] + nums[k] = -nums[i]
+                if nums[j] + nums[k] < -nums[i] and j < k:
+                    j += 1
+                if nums[j] + nums[k] > -nums[i] and j < k:
+                    k -= 1
+                # print([i,j,k])
+                if nums[j] + nums[k] == -nums[i] and j < k:
+                    hash = numToStr(nums[i]) + numToStr(nums[j]) + numToStr(nums[k])
+                    if not resHash.get(hash):
+                        resHash[hash] = True
+                        res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+        return res
