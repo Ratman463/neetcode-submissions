e766647158206e10b4b7f13e6bce1692a4c79db8
Ratman <wurenmooncake@gmail.com>
2026-06-12 18:36:04 +0800

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        # binsearch k
+        def canEatingUpWithK(piles: List[int], h: int, k: int) -> bool:
+            total_h = sum([math.ceil(pile / k) for pile in piles])
+            return total_h <= h
+
+        left, right = 1, max(piles) + 1
+        mid = (left + right) // 2
+        best = right
+
+        while left < right:
+            mid = (left + right) // 2
+            if canEatingUpWithK(piles, h, mid):
+                right = mid
+                best = min(best, mid)
+            else:
+                left = mid + 1
+        return best
