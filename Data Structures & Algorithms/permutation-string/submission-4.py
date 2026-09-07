0fdd53b075dbd33ffd803e311b3a07ab981e9917
Ratman <wurenmooncake@gmail.com>
2026-09-07 15:54:23 +0800

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isSameDick(self, arr1, arr2) -> bool:
+        return arr1 == arr2
+        
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        dicks = [0] * 26
+        new_dicks = [0] * 26
+
+        l1, l2 = len(s1), len(s2)
+        if l1 > l2:
+            return False
+
+        for i in range(l1):
+            dicks[ord(s1[i]) - ord('a')] += 1
+            new_dicks[ord(s2[i]) - ord('a')] += 1
+        
+        if self.isSameDick(dicks, new_dicks):
+            return True
+
+        for i in range(1, l2 - l1 + 1):            
+            newChar = ord(s2[i + l1 - 1]) - ord('a')
+            oldChar = ord(s2[i - 1]) - ord('a')
+
+            new_dicks[newChar] += 1
+            new_dicks[oldChar] -= 1
+
+            # print(i)
+            # print(dicks)
+            # print(new_dicks)
+
+            if self.isSameDick(dicks, new_dicks):
+                return True
+        
+        return False
