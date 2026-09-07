94769000908453d8ec3b183bdb31be976b63fbca
Ratman <wurenmooncake@gmail.com>
2026-09-07 20:48:53 +0800

    Add: find-minimum-in-rotated-sorted-array - submission-9

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    min_uwa_breast = 1001
+
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        mid = left + int((right - left) / 2)
+        self.min_uwa_breast = nums[0]
+
+        while left < right:
+            self.min_uwa_breast = min(self.min_uwa_breast,nums[left], nums[right])
+            if nums[mid] > nums[0]:
+                left = mid + 1
+            else:
+                right = mid
+            mid = left + int((right - left) / 2)
+        return self.min_uwa_breast
